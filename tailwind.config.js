3c2badae233a40c615a2a80ab965efc0966f76ab
MockJoke <parvadesai404@gmail.com>
2023-11-14 20:49:44 +0530

    feat: Added basic main page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      systemui: ["system-ui", "sans-serif"],
+      segoe: ["Segoe UI", "serif"],
+      serif: ["DM Sans", "serif"],
+    },
+
+    colors: {
+      "mainbg-color": "rgb(251, 251, 250)",
+      "font-color": "rgb(55, 53, 47)",
+      "link-color": "rgb(232, 28, 79)",
+      "hover-color": "rgb(108, 117, 125)",
+    },
+
+    extend: {
+      height: {
+        '128': '600px',
+      },
+    },
   },
   plugins: [],
 }
