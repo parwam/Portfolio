bd049d36f1565d4d8d8dfd2bde419238637c7630
kharva <parvadesai404@gmail.com>
2024-05-04 19:17:01 +0530

    [Add] new projects page [Add] new colors in config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./*.html"],
+  content: ["./src/**/*.{html,js}", "./*.html", "projects/*.html"],
 
   darkMode: "class",
 
@@ -18,13 +18,34 @@ module.exports = {
       "font-color-dark": "rgb(235, 235, 235)",
       "link-color": "rgb(232, 28, 79)",
       "hover-color": "rgb(108, 117, 125)",
+      "tags-color": "rgb(134, 136, 138)",
       "navbar-color": "rgb(244, 242, 233)",
       "navbar-color-dark": "rgb(18, 18, 18)",
+      "color-blue": "#55aff9",
+      "color-blue-dark": "#146EBE",
+      "color-yellow": "#FFD43B",
+      "color-yellow-dark": "#FAB005",
+      "color-purple": "#B197FC",
+      "color-purple-dark": "#6741D9",
+      "color-pink": "#E599F7",
+      "color-pink-dark": "#9C36B5",
+      "color-red": "#FF8787",
+      "color-red-dark": "#E03131",
+      "color-green": "#63E6BE",
+      "color-green-dark": "#0CA678",
     },
 
     extend: {
       height: {
         128: "600px",
+      },
+
+      borderWidth: {
+        '6': "6px"
+      },
+
+      maxWidth: {
+        'mdsm': "344px"
       },
     },
   },
